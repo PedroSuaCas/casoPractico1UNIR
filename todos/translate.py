81ce1d7d74a894dea64abcbca92f5d8dc0099a15
EC2 Default User <ec2-user@ip-172-31-49-210.ec2.internal>
2021-05-22 13:09:02 +0000

    Desarrollo translate - Falta Rol

--- todos/translate.py
+++ todos/translate.py
@@ -6,21 +6,25 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 translateService = boto3.client(service_name='translate')
 
-
-def get(event, context):
+def translate(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
     # fetch todo from the database
     result = table.get_item(
         Key={
-            'id': event['pathParameters']['id']
+            'id': event['pathParameters']['id'],
+            'languague': event['pathParameters']['languague']
         }
     )
+    resultFinal = translateService.translate_text(Text=result.Key.id,
+                                  SourceLanguageCode="auto",
+                                  TargetLanguageCode=result.Key.languague);
+    
 
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(result['Item'],
+        "body": json.dumps(resultFinal['Item'],
                            cls=decimalencoder.DecimalEncoder)
     }
 

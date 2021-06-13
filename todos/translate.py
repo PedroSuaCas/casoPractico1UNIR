fc59e3ee611cfd4ecabaff0395903ff650259fd2
EC2 Default User <ec2-user@ip-172-31-40-32.ec2.internal>
2021-06-13 17:04:23 +0000

    tranlaste

--- todos/translate.py
+++ todos/translate.py
@@ -3,37 +3,38 @@ import json
 
 from todos import decimalencoder
 import boto3
+
 dynamodb = boto3.resource('dynamodb')
 translateService = boto3.client(service_name='translate')
 
-def translate(event, context):
+def get (event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
     # fetch todo from the database
     result = table.get_item(
         Key={
-            'id': event['pathParameters']['id'],
-            'languague': event['pathParameters']['languague']
+            'id': event['pathParameters']['id']
         }
     )
     
-    if result.Key.languague == 'en':
+    if event['pathParameters']['languague'] == 'en':
         target = 'en'
-    elif result.Key.languague == 'fr':
+    elif event['pathParameters']['languague'] == 'fr':
         target = 'fr'
     else:
         target = 'auto'
-        
-    resultFinal = translateService.translate_text(Text = result.Key.id,
-                                  SourceLanguageCode ="auto",
-                                  TargetLanguageCode = target);
     
+    resultFinal = translateService.translate_text(Text = result['Item']['text'], SourceLanguageCode='auto', TargetLanguageCode=target)
+                                                                          
 
-    # create a response
+    result['Item']["text"] = resultFinal.get('TranslatedText')
+
+    #create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(resultFinal['Item'],
+        "body": json.dumps(result['Item'],
                            cls=decimalencoder.DecimalEncoder)
     }
 
     return response
+    
